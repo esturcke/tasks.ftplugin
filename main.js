f0ca906f9b55e85b30a75da9aaa69c7e93546f1e
Erik J. Sturcke <erik@carbonbox.com>
2013-06-13 21:46:14 -0400

    Add progress calculations
    This tends to crash a lot as it is.

--- main.js
+++ main.js
@@ -11,7 +11,18 @@ define(function(require, exports, module) {
 		model.endUpdates();
 	}
 
+	function getNum(node, tag) {
+		return parseFloat(node.tag(tag)) || 0;
+	}
+
 	function updateNode(node) {
+		// handle remaining calculation
+		var estimate = getNum(node, 'estimate');
+		var actual   = getNum(node, 'actual');
+		var progress = node.tag('done') ? 1 : getNum(node, 'progress') / 100;
+		var remaining = progress > 0 ? Math.round( 10 * actual * (1 - progress) / progress ) / 10 : estimate;
+		remaining > 0 ? node.addTag('remaining', remaining) : node.removeTag('remaining');
+
 		node = node.parent;
 		if (!node || node.type() != 'unordered') return;
 		var children = node.children();
@@ -20,11 +31,19 @@ define(function(require, exports, module) {
 				var sum = sumTag(children, tag);
 				sum > 0 ? node.addTag(tag, sum) : node.removeTag(tag);
 			});
+
+			var progress = Math.round(averageTag(children, 'progress', 'estimate'));
+			progress > 0 ? node.addTag('progress', progress + '%') : node.removeTag('progress');
 		}
 	}
 
 	function sumTag(nodes, tag) {
-		return _.reduce(nodes, function(sum, node) { return sum + (parseFloat(node.tag(tag)) || 0) }, 0);
+		return _.reduce(nodes, function(sum, node) { return sum + getNum(node, tag) }, 0);
+	}
+
+	function averageTag(nodes, tag, weight) {
+		var weightSum = sumTag(nodes, weight);
+		return weightSum > 0 ? _.reduce(nodes, function(sum, node) { return sum + getNum(node, weight) * getNum(node, tag) }, 0) / weightSum : 0;
 	}
 
 	exports.editorDidLoad = function(editor) {
